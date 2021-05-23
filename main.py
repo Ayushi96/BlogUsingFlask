def40dad637a3d3d0c5e44d84027dbfdaef3e77b
Ayushi Vishwakarma <cs17mtech11007@iith.ac.in>
2021-05-24 00:19:28 +0530

    add database connection for Contacts table

--- main.py
+++ main.py
@@ -1,47 +1,63 @@
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+import json
+
+local_server = True
+
+with open('config.json', 'r') as c:
+    params = json.load(c)['params']
 
 app = Flask(__name__)
+
+if (local_server):
+    app.config['SQLALCHEMY_DATABASE_URI'] = params['local_uri']
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = params['prod_uri']
+
+# initialization
+db = SQLAlchemy(app)
+
+class Contacts(db.Model):
+    '''
+        sno, name, phone_num, msg, date, email
+    '''
+    sno = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    phone_num = db.Column(db.String(12),  nullable=False)
+    msg = db.Column(db.String(120), nullable=False)
+    date = db.Column(db.String(12), nullable=True)
+    email = db.Column(db.String(20), nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
 
 @app.route('/')
 def home():
     user = 'Ayushi'
-    return render_template('index.html', name=user)
+    return render_template('index.html', params=params)
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    return render_template('about.html', params=params)
     
-@app.route('/contact')
+@app.route('/contact', methods=['GET', 'POST'])
 def contact():
-    return render_template('contact.html')
+    if request.method == 'POST':
+        ''' Add entry to the database '''
+        name = request.form.get('name')
+        email = request.form.get('email')
+        phone = request.form.get('phone')
+        msg = request.form.get('msg')
+ 
+        entry = Contacts(name=name, phone_num=phone, msg=msg, date= datetime.now(), email=email)
+        db.session.add(entry)
+        db.session.commit()
 
-@app.route('/armstrong/<int:n>')
-def armstrong(n):
-    # n = int(input("Enter the number "))
-    sum = 0 
-    order = len(str(n))
-    copy_n = n
-    
-    while(n > 0):
-        d = n%10
-        sum += d ** order
-        n = n//10
-    print(f"sum = {sum}")
-    if copy_n == sum:
-        print(f"{copy_n} is an armstrong number")
-        result = {
-            "Number": copy_n, 
-            "Armstrong": True,
-            "Author": "Ayushi"
-        }
-    else:
-        print(f"{copy_n} is not an armstrong number")
-        result = {
-            "Number": copy_n, 
-            "Armstrong": False,
-            "Author": "Ayushi"
-        }
-    return jsonify(result)
+
+    return render_template('contact.html', params=params)
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)

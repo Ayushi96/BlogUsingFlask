f63759fa629edd53ba35bf4ab22ab7ec89cc950f
Ayushi Vishwakarma <cs17mtech11007@iith.ac.in>
2021-06-28 00:00:34 +0530

    insert and edit post using dashboard

--- main.py
+++ main.py
@@ -1,15 +1,29 @@
-from flask import Flask, jsonify, render_template, request
+from flask import Flask, jsonify, render_template, request, session
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 import json
+from flask_mail import Mail
+from werkzeug.utils import redirect
 
-local_server = True
 
 with open('config.json', 'r') as c:
     params = json.load(c)['params']
 
-app = Flask(__name__)
+local_server = params['local_server']
 
+app = Flask(__name__)
+# for security in sessions you need to set the secret key
+app.secret_key = 'super-secret-key'
+
+app.config.update(
+    MAIL_SERVER = 'smtp.gmail.com',
+    MAIL_PORT = '465',
+    MAIL_USE_SSL = True,
+    MAIL_USERNAME = params['gmail-user'],
+    MAIL_PASSWORD = params['gmail-password'] 
+    
+)
+mail = Mail(app)
 if (local_server):
     app.config['SQLALCHEMY_DATABASE_URI'] = params['local_uri']
 else:
@@ -32,10 +46,82 @@ class Contacts(db.Model):
     def __repr__(self):
         return '<User %r>' % self.username
 
+
+class Posts(db.Model):
+    '''
+        sno, title, slug, content, date
+    '''
+    sno = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80), nullable=False)
+    tagline  = db.Column(db.String(80), nullable=False)
+    slug = db.Column(db.String(25),  nullable=False)
+    content = db.Column(db.String(120), nullable=False)
+    img_file = db.Column(db.String(120), nullable=False)
+    date = db.Column(db.String(12), nullable=True)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
 @app.route('/')
 def home():
     user = 'Ayushi'
-    return render_template('index.html', params=params)
+    posts = Posts.query.filter_by().all()[0: params['no_of_posts']]
+    return render_template('index.html', params=params, user=user, posts=posts)
+
+@app.route('/dashboard', methods=['GET', 'POST'])
+def dashboard():
+    # if the user is already logged in 
+    if 'user' in session and session['user'] == params['admin_user']:
+        # provide access
+        posts = Posts.query.all()
+        return render_template('dashboard.html', params=params, posts=posts)
+
+    if request.method == 'POST':
+        # redirect to admin panel
+        username = request.form.get('uname')
+        userpass =  request.form.get('pass')
+        if username == params['admin_user'] and userpass == params['admin_password']:
+            # set the session variable 
+            session['user'] = username  # basically telling the flask app that this user is logged in 
+            posts = Posts.query.all()
+            return render_template('dashboard.html', params=params, posts=posts)
+            
+    else:
+        return render_template('login.html', params=params)
+
+@app.route('/edit/<string:sno>', methods=['GET', 'POST'])
+def edit(sno):
+    if 'user' in session and session['user'] == params['admin_user']:
+        if request.method == 'POST':
+            title = request.form.get('title')
+            tagline = request.form.get('tagline')
+            slug = request.form.get('slug')
+            content = request.form.get('content')
+            img_file = request.form.get('img_file')
+            date = datetime.now()
+
+            if sno == '0':
+                post = Posts(title=title, tagline=tagline, slug=slug, content=content, img_file=img_file, date=date)
+                db.session.add(post)
+                db.session.commit()
+            else:
+                try:
+                    int_sno = int(sno)
+                    edit_post = Posts.query.filter_by(sno=int_sno).first_or_404(description='There is no post with sno {}'.format(sno))
+                    edit_post.title = title
+                    edit_post.tagline = tagline
+                    edit_post.slug = slug
+                    edit_post.content = content
+                    edit_post.img_file = img_file
+                    edit_post.date = date
+                    db.session.commit()
+                    return redirect('/edit/'+sno)
+                except:
+                    print("sno provided was not NAN")
+        post = Posts.query.filter_by(sno=sno).first()            
+                
+        return render_template('edit.html', params=params, post=post)
+
 
 @app.route('/about')
 def about():
@@ -53,10 +139,19 @@ def contact():
         entry = Contacts(name=name, phone_num=phone, msg=msg, date= datetime.now(), email=email)
         db.session.add(entry)
         db.session.commit()
+        mail.send_message('New message from ' + name, 
+        sender=email, 
+        recipients=[params['gmail-user']],
+        body= msg + '\n' + phone
+        )
 
 
     return render_template('contact.html', params=params)
 
+@app.route('/post/<string:post_slug>', methods=['GET'])
+def get_post(post_slug):
+    post = Posts.query.filter_by(slug=post_slug).first()
+    return render_template('post.html', params=params, post=post)
 
 
 if __name__ == '__main__':
